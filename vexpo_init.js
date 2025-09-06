27c1d611ab7743955e931323c17b1e89d8bbfa2a
jikiweng <94891522+jikiweng@users.noreply.github.com>
2025-09-06 15:00:13 +0900

    Add files via upload

--- vexpo_init.js
+++ vexpo_init.js
@@ -12,39 +12,13 @@ window.addEventListener('load', function() {
         document.body.classList.add('meta-quest');
         
         setTimeout(() => {
-            setupScrollbar();
+            setupScrollbarForQuest();
         }, 200);
     } 
     else{
-        const originalWrapper = document.querySelector('#content-wrapper');
-        const scrollContent = originalWrapper.querySelector('.simplebar-content');
-        const bodyHeight = scrollContent.scrollHeight;
-        const windowHeight = window.innerHeight
-        const bottomPoint = bodyHeight - windowHeight
-        console.log("bodyHeight:", bodyHeight, "windowHeight:", windowHeight, "bottomPoint:", bottomPoint);
-        
-        var url = window.location.href;
-        var tsVer = document.head.querySelector('[name=ts-ver][content]') ? document.head.querySelector('[name=ts-ver][content]').content : "";
-        var ppVer = document.head.querySelector('[name=pp-ver][content]') ? document.head.querySelector('[name=pp-ver][content]').content : "";
-        var cpVer = document.head.querySelector('[name=cp-ver][content]') ? document.head.querySelector('[name=cp-ver][content]').content : "";
-        var pdVer = document.head.querySelector('[name=pd-ver][content]') ? document.head.querySelector('[name=pd-ver][content]').content : "";
-        var tfVer = document.head.querySelector('[name=tf-ver][content]') ? document.head.querySelector('[name=tf-ver][content]').content : "";
-        var termInfo = url + "?ts-ver=" + tsVer + "&pp-ver=" + ppVer + "&cp-ver=" + cpVer + "&pd-ver=" + pdVer + "&tf-ver=" + tfVer;
-        
-        if(bodyHeight <= windowHeight) {
-            scrolledTermInfo = termInfo
-        }
-        
-        window.addEventListener('scroll', () => {
-            const scrollContainer = originalWrapper.querySelector('.simplebar-content-wrapper');
-            const scrollTop = scrollContainer.scrollTop;
-            console.log("currentPos: ",scrollTop );
-
-            if (bottomPoint <= scrollTop ) {
-                scrolledTermInfo = termInfo
-            }
-        })
-        console.log("scrolledTermInfo:", scrolledTermInfo);
+        setTimeout(() => {
+            setupScrollbarForNonVR();
+        }, 200);
     }
 });
 
@@ -60,7 +34,39 @@ function isMetaQuestBrowser() {
     return isQuest;
 }
 
-function setupScrollbar() {    
+function setupScrollbarForNonVR(){
+    const originalWrapper = document.querySelector('#content-wrapper');
+    const scrollContent = originalWrapper.querySelector('.simplebar-content');
+    const bodyHeight = scrollContent.scrollHeight;
+    const windowHeight = window.innerHeight
+    const bottomPoint = bodyHeight - windowHeight
+    console.log("bodyHeight:", bodyHeight, "windowHeight:", windowHeight, "bottomPoint:", bottomPoint);
+    
+    var url = window.location.href;
+    var tsVer = document.head.querySelector('[name=ts-ver][content]') ? document.head.querySelector('[name=ts-ver][content]').content : "";
+    var ppVer = document.head.querySelector('[name=pp-ver][content]') ? document.head.querySelector('[name=pp-ver][content]').content : "";
+    var cpVer = document.head.querySelector('[name=cp-ver][content]') ? document.head.querySelector('[name=cp-ver][content]').content : "";
+    var pdVer = document.head.querySelector('[name=pd-ver][content]') ? document.head.querySelector('[name=pd-ver][content]').content : "";
+    var tfVer = document.head.querySelector('[name=tf-ver][content]') ? document.head.querySelector('[name=tf-ver][content]').content : "";
+    var termInfo = url + "?ts-ver=" + tsVer + "&pp-ver=" + ppVer + "&cp-ver=" + cpVer + "&pd-ver=" + pdVer + "&tf-ver=" + tfVer;
+        
+    if(bodyHeight <= windowHeight) {
+        scrolledTermInfo = termInfo
+    }
+
+    const scrollContainer = originalWrapper.querySelector('.simplebar-content-wrapper');     
+    scrollContainer.addEventListener('scroll', () => {        
+        const scrollTop = scrollContainer.scrollTop;
+        console.log("currentPos: ",scrollTop );
+
+        if (bottomPoint <= scrollTop ) {
+            scrolledTermInfo = termInfo
+        }
+    })
+    console.log("scrolledTermInfo:", scrolledTermInfo);
+}
+
+function setupScrollbarForQuest() {    
     const originalWrapper = document.querySelector('#content-wrapper');
     
     if (!originalWrapper) {
